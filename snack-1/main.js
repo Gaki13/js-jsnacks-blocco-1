f460f285f440a39c46bcd9d4264cf4877ee85b5a
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 11:35:19 +0200

    aggiunta condizione per dichiare il numero più grande

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,5 +1,9 @@
 // 1. L'utente inserisce 2 numeri in successione
 const firstNumber = prompt("inserisci un numero:");
 const secondNumber = prompt("inserisci un'altro numero:")
-console.log(firstNumber, secondNumber);
 // 2. Stampo il maggiore in console
+if (firstNumber < secondNumber){
+    console.log(secondNumber)
+} else {
+    console.log(firstNumber)
+}
