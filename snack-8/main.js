15e65061105c6b390a01ad7793b1ecb53c236f52
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 17:42:25 +0200

    creo la mia variabile somma

--- snack-8/main.js
+++ snack-8/main.js
@@ -1,4 +1,14 @@
 //1. Crea un'array di numeri interi
 const list = ["1", "2", "3", "4", "5", "6", "7", "8"]
 console.log(list);
-//2. Sommo tutti gli elementi in posizione dispari
+
+//2. Creo una variabile che avrà il valore della mia somma
+let somma = 0;
+
+//3. Sommo tutti gli elementi in posizione dispari
+    //utilizzo un ciclo per scremare i numeri in posizione dispari
+    for ( let i = 0; i < list.length; i++){
+        if( list[ i % 2 === 1]){
+            console.log(i);
+        };
+    };
