0f0d6e67c94a940f5ade87a9ae52410dd428259a
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 12:05:06 +0200

    chiedi i 10 numeri all'utente

---
+++ snack-3/main.js
@@ -0,0 +1,12 @@
+//1. Chiedo all'utente un numero per 10 volte
+const firstNumber = prompt("inserisci un numero")
+const secondtNumber = prompt("inserisci un numero")
+const thirdNumber = prompt("inserisci un numero")
+const fourtNumber = prompt("inserisci un numero")
+const fiveNumber = prompt("inserisci un numero")
+const sixtNumber = prompt("inserisci un numero")
+const sevenNumber = prompt("inserisci un numero")
+const eightNumber = prompt("inserisci un numero")
+const nineNumber = prompt("inserisci un numero")
+const tenNumber = prompt("inserisci un numero")
+//2. Sommo tutti i numeri inseriti
