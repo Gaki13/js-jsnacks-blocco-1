037651bdebd9e5af40eb670d8aa252586559ffdd
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 12:31:05 +0200

    somma i risultati

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,12 +1,16 @@
 //1. Chiedo all'utente un numero per 10 volte
-const firstNumber = prompt("inserisci un numero")
-const secondtNumber = prompt("inserisci un numero")
-const thirdNumber = prompt("inserisci un numero")
-const fourtNumber = prompt("inserisci un numero")
-const fiveNumber = prompt("inserisci un numero")
-const sixtNumber = prompt("inserisci un numero")
-const sevenNumber = prompt("inserisci un numero")
-const eightNumber = prompt("inserisci un numero")
-const nineNumber = prompt("inserisci un numero")
-const tenNumber = prompt("inserisci un numero")
+const firstNumber = Number( prompt("inserisci un numero"));  
+const secondtNumber = prompt("inserisci un numero");
+const thirdNumber = prompt("inserisci un numero");
+const fourtNumber = prompt("inserisci un numero");
+const fiveNumber = prompt("inserisci un numero");
+const sixtNumber = prompt("inserisci un numero");
+const sevenNumber = prompt("inserisci un numero");
+const eightNumber = prompt("inserisci un numero");
+const nineNumber = prompt("inserisci un numero");
+const tenNumber = prompt("inserisci un numero");
+
+console.log(firstNumber, secondtNumber, thirdNumber, fourtNumber, fiveNumber, sixtNumber,sevenNumber, eightNumber, nineNumber, tenNumber)
 //2. Sommo tutti i numeri inseriti
+const somma = firstNumber.value + secondtNumber.value
+console.log(somma)
