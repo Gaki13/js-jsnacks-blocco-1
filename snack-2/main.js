5239b0967e09ce45adb5388b7b0585dda4a14ac0
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 11:53:43 +0200

    aggiungo condizione per il confronto e stampo in console

--- snack-2/main.js
+++ snack-2/main.js
@@ -3,3 +3,10 @@ const firstWordl = prompt("inserisci una parola")
 const secondWordl = prompt("inserisci un'altra parola ancora")
 //2. Definisco quale delle 2 parole è più corta
 //3. Stampo prima la parola più corta e poi quella più lunga
+if (firstWordl.length < secondWordl.length){
+    console.log(firstWordl, secondWordl);
+} else if (firstWordl.length > secondWordl.length){
+    console.log(secondWordl, firstWordl)
+} else {
+    console.log("sono lunghe uguali")
+}
