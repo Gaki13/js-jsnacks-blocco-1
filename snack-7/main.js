7e9e1d1692726fe9f04b0c518f47cd3f6e7c73d5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 16:50:07 +0200

    aggiunta array per nomi e congnomi

---
+++ snack-7/main.js
@@ -0,0 +1,5 @@
+//1. Creo 2 array da cui prendere gli elementi e creare i nomi 
+const names = ["john", "Narancia", "Bruno", "Jonathan", "Josuke", "Jean", "Panda"]
+const lastNames = ["Joestar", "Bucciarati", "Picasso", "Jeager", "Lopez", "Ccontainer"]
+//2. Creo di math random dai quali scegliere i valori dei miei array
+//3. Connetto le mie 2 stringhe per creare il nome che poi stamperò in console
