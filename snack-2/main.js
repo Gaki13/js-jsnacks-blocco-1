85a9e6f249060ea1616cf14fd8a61d7d944a7684
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 11:46:29 +0200

    aggiunta richiesta delle 2 parole

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,3 +1,5 @@
 //1. Chiedo all'utente 2 parole in succesione
+const firstWordl = prompt("inserisci una parola")
+const secondWordl = prompt("inserisci un'altra parola ancora")
 //2. Definisco quale delle 2 parole è più corta
 //3. Stampo prima la parola più corta e poi quella più lunga
