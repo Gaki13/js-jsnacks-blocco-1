d9685ca06888a1fc590063b9915b2fa6a395cdfb
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 16:23:25 +0200

    chiedo un numero all'user

---
+++ snack-6/main.js
@@ -0,0 +1,4 @@
+//1. Chiedo un numero all'user
+const numberUser = Number(prompt("inserisci un numero"))
+//2. SE il numero è pari, lo stampo
+//   ALTRIMENTI stampo il numero successivo a quello inserito
