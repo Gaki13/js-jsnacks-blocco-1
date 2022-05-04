5a7a6d7ec72add0b6c8cb7653e418ac7fdba5512
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 16:27:12 +0200

    creo condizione di controllo e stampo il numero successivo a quelli dispari

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,4 +1,10 @@
 //1. Chiedo un numero all'user
-const numberUser = Number(prompt("inserisci un numero"))
+let numberUser = Number(prompt("inserisci un numero"))
+console.log(numberUser)
 //2. SE il numero è pari, lo stampo
+if (numberUser % 2 === 0){
+    console.log(numberUser)
+} else if (numberUser % 2 === 1){
+    console.log(++numberUser)
+};
 //   ALTRIMENTI stampo il numero successivo a quello inserito
