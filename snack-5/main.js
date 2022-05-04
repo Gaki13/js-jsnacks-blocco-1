a7c860f0aad1d458e2b79c020f76717086070379
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 12:39:39 +0200

    chiedo all'utente un numero 6 volte

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,4 +1,9 @@
 //1. Crea un'array vuoto
 const lista = [];
+
 //2. Chiedi all'utente di inserire un numero 6 volte
+for ( let i = 1; i <= 6; i++ ){
+    const number = Number(prompt("inserisci un numero"))
+};
+
 //3. SE il numero inserito è dispari il numero va inserito nell'array vuoto
