c8044b735afc6566b15f4185211a8b83b54f8542
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-04 11:33:19 +0200

    aggiunti prompt per chiedere all'utente i numeri

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,1 +1,5 @@
-
+// 1. L'utente inserisce 2 numeri in successione
+const firstNumber = prompt("inserisci un numero:");
+const secondNumber = prompt("inserisci un'altro numero:")
+console.log(firstNumber, secondNumber);
+// 2. Stampo il maggiore in console
